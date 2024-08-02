451f43bc3ef0f43a5c6943b8ea47ed36b0445d41
Aries1711 <levfreey17@gmail.com>
2024-08-02 23:25:22 +0700

    feat: theme change, motion on scroll progress

---
+++ components/section/Header.jsx
@@ -0,0 +1,11 @@
+import ThemeToggler from "../themes/ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
---
+++ hooks/useScrollProgress.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+function useScrollProgress() {
+  const [completion, setCompletion] = useState(0);
+  useEffect(() => {
+    const updateScrollCompletion = () => {
+      const currentProgress = window.scrollY;
+      const scrollHeight = document.body.scrollHeight - window.innerHeight;
+
+      if (scrollHeight) {
+        setCompletion(Number(currentProgress / scrollHeight).toFixed(2) * 100);
+      }
+    };
+    window.addEventListener("scroll", updateScrollCompletion);
+    return () => window.removeEventListener("scroll", updateScrollCompletion);
+  }, []);
+  return completion;
+}
+
+export default useScrollProgress;
