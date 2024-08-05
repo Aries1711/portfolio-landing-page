5e84829ec98979edc8ae5f77cc0bb124c0cc8818
Aries1711 <levfreey17@gmail.com>
2024-08-05 16:20:11 +0700

    feat : mobile navigation

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/layouts/Hero";
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
--- components/layouts/MobileNav.jsx
+++ components/layouts/MobileNav.jsx
@@ -1,5 +1,30 @@
-function MobileNav() {
-  return <div>MobileNav</div>;
-}
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+
+import Nav from "./Nav";
+import Logo from "./Logo";
+import Socials from "../contents/Socials";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-painter" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+          </div>
+          <Socials containerStyles="flex gap-x-4" IconsStyles="text-2xl" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileNav;
